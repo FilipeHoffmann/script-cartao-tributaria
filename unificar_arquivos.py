c4602fe4544fcf1615f6d38c1ee418dae9c0801b
Filipe Antonio Hoffmann <filipeantoniohoffmann@gmail.com>
2024-05-09 12:00:50 -0300

    Separação de cada elemento da lista

--- unificar_arquivos.py
+++ unificar_arquivos.py
@@ -10,5 +10,5 @@ class unificar_arquivos:
         for arquivo in self.arquivos.retornar_arquivos():
             coletar = coletar_dados_txt("arquivos_do_mes\\" + arquivo)
             for coleta in coletar.instanciar_dados():
-                self.arquivos_gerais.append(coleta)
+                self.arquivos_gerais.append(coleta.split(";"))
         return self.arquivos_gerais
