c6ffb2a0daaba7913c0c154ddb449dbf465941f6
Filipe Antonio Hoffmann <filipeantoniohoffmann@gmail.com>
2024-05-09 19:10:59 -0300

    Função somar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from arquivos_pasta import arquivos_pasta
 from unificar_arquivos import unificar_arquivos
 from unificar_por_cnpj import unificar_por_cnpj
+from somar_credito_debito import somar_credito_debito
 
 if __name__ == "__main__":
     arquivo = arquivos_pasta("arquivos_do_mes")
@@ -8,6 +9,6 @@ if __name__ == "__main__":
     dados_unificados = unificar.unificar()
     unificado_cnpj = unificar_por_cnpj(dados_unificados)
     cnpj = unificado_cnpj.unificar()
-    for i in range(len(cnpj)):
-        print(cnpj[i])
+    soma = somar_credito_debito(cnpj)
+    total = soma.somar()
     pass
---
+++ somar_credito_debito.py
@@ -0,0 +1,10 @@
+class somar_credito_debito:
+    def __init__(self,lista_cnpj_valor:list) -> None:
+        self.lista_cnpj_valor = lista_cnpj_valor
+
+    def somar(self):
+        for i in range(len(self.lista_cnpj_valor)):
+            self.lista_cnpj_valor[i][1] = float(self.lista_cnpj_valor[i][1]) + float(self.lista_cnpj_valor[i][2])
+            self.lista_cnpj_valor[i].pop()
+            print(self.lista_cnpj_valor[i])
+        return self.lista_cnpj_valor
