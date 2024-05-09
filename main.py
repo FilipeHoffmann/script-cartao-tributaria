9cd8c2a2bdbdab2c59d0b3f0def3b7fd770e44bb
Filipe Antonio Hoffmann <filipeantoniohoffmann@gmail.com>
2024-05-09 13:57:24 -0300

    Teste

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from unificar_arquivos import unificar_arquivos
 if __name__ == "__main__":
     arquivo = arquivos_pasta("arquivos_do_mes")
     unificar = unificar_arquivos(arquivo)
-    print(unificar.unificar())
+    for i in range(len(unificar.unificar())):
+        print(unificar.unificar()[i])
     pass
