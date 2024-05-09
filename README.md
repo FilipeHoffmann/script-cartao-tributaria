b29d19398c42712e0024e775651b993b75b95a25
Filipe Antonio Hoffmann <filipeantoniohoffmann@gmail.com>
2024-05-08 22:23:32 -0300

    Otimização do arquivo main

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # bot-cartao-tributaria
+
 Bot para a formatação dos dados fornecidos pelo SEFAZ.
 
-- Converter os dados presentes no .txt para uma planilha.
+- Converter os dados presentes nos arquivos .txt para uma planilha.
 - Somar todos os valores (crédito e débito) por CNPJ sem distinção de banco.
--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import coletar_dados_txt
 from arquivos_pasta import arquivos_pasta
 from unificar_arquivos import unificar_arquivos
 if __name__ == "__main__":
-    """arquivos = arquivos_pasta.arquivos_pasta("arquivos_do_mes")
-    for arquivo in arquivos.retornar_arquivos():
-        dados_txt:list = coletar_dados_txt.coletar_dados_txt("arquivos_do_mes\\"+arquivo)
-        for dado in dados_txt.instanciar_dados():
-            print(dado)"""
-    
     arquivo = arquivos_pasta("arquivos_do_mes")
     unificar = unificar_arquivos(arquivo)
     print(unificar.unificar())
