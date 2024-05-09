83f1b6f390675a6c57668a9eedb928c4aedc1624
Filipe Antonio Hoffmann <filipeantoniohoffmann@gmail.com>
2024-05-09 20:00:26 -0300

    Final

--- somar_credito_debito.py
+++ somar_credito_debito.py
@@ -6,4 +6,6 @@ class somar_credito_debito:
         for i in range(len(self.lista_cnpj_valor)):
             self.lista_cnpj_valor[i][1] = float(self.lista_cnpj_valor[i][1]) + float(self.lista_cnpj_valor[i][2])
             self.lista_cnpj_valor[i].pop()
+            self.lista_cnpj_valor[i][0] = str(f"'{self.lista_cnpj_valor[i][0]}")
+            self.lista_cnpj_valor[i][1] = str(self.lista_cnpj_valor[i][1]).replace(".",",")
         return self.lista_cnpj_valor
