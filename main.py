341d7e2a17f3ee76a148e9038d48ea40cf6469d4
Filipe Antonio Hoffmann <filipeantoniohoffmann@gmail.com>
2024-05-09 14:08:30 -0300

    otimização

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from unificar_arquivos import unificar_arquivos
 if __name__ == "__main__":
     arquivo = arquivos_pasta("arquivos_do_mes")
     unificar = unificar_arquivos(arquivo)
-    for i in range(len(unificar.unificar())):
-        print(unificar.unificar()[i])
+    dados_unificados = unificar.unificar()
+    for i in range(len(dados_unificados)):
+        print(dados_unificados[i])
     pass
